6c14722f26bc4aec9462e06957311236d86da54c
Nico M <nico_16bcn@hotmail.com>
2022-06-27 18:36:14 +0200

    iteration 3

--- routes/drones.js
+++ routes/drones.js
@@ -13,14 +13,17 @@ router.get('/drones', (req, res, next) => {
     .catch(error => console.log(error));
 });
 
-router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
-});
+router.get('/drones/create', (req, res, next) => res.render('drones/create-form.hbs'));
+
 
 router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
+  console.log(req.body);
+  const { name, propellers, maxSpeed } = req.body;
+
+  Drone.create({ name, propellers, maxSpeed })
+    /*.then(droneFromDB => console.log(`New drone created: ${droneFromDB.name}.` */
+    .then(() => res.redirect('/drones'))
+    .catch(error => res.redirect('/drones/create'));
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
